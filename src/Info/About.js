975022eaa77b612ec76d81d0f9e868e5b007c206
Karolis Kazlauskis <karolis@flumens.io>
2020-05-08 09:51:37 +0100

    Add credits page texts

--- src/Info/About.js
+++ src/Info/About.js
@@ -5,19 +5,19 @@ const { P } = Section;
 
 export default () => (
   <Page id="about">
-    <Header title="About" i18n />
+    <Header title="About" />
     <Main class="ion-padding">
       <Section>
-        <P i18n>
+        <P>
           Dynamic Dunescapes is a fantastic new project to restore sand dunes
           across England and Wales for the benefit of people, communities and
           wildlife. Sand dunes in the UK look very different today.
         </P>
-        <P i18n>
+        <P>
           Gone are the sandy features, which made homes for special creatures,
           so let’s bring them back, to stay!
         </P>
-        <P i18n>
+        <P>
           Picnics in a sheltered hollow, hide and seek... sand dunes are a
           familiar and natural playground. Less well known is their role as a
           sanctuary for endangered plants and animals like the fen orchid and
@@ -26,7 +26,7 @@ export default () => (
           needs areas of open sand to thrive. Healthy sand dunes need to move
           and be dynamic.
         </P>
-        <P i18n>
+        <P>
           The Dynamic Dunescapes project is big and ambitious – targeting some
           of the most important sand dune systems across England and Wales. We
           will work with schools and local groups, volunteers and visitors of
---
+++ src/Info/Credits/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { IonList, IonItem } from '@ionic/react';
+import { Header, Page, Main, Section } from '@apps';
+import './styles.scss';
+import './sponsors.png';
+import './flumens.png';
+
+const { P, H } = Section;
+
+export default () => (
+  <Page id="credits">
+    <Header title="Credits" />
+    <Main class="ion-padding">
+      <IonList lines="none">
+        <IonItem>
+          <img src="/images/sponsors.png" alt="" />
+        </IonItem>
+      </IonList>
+
+      <Section>
+        <H>People</H>
+        <P>
+          We are very grateful for all the people that helped to create this
+          app:
+        </P>
+        <P skipTranslation>
+          <b>Laurence Jones</b> (UK Centre for Ecology & Hydrology)
+        </P>
+        <P skipTranslation>
+          <b>Tim Braund</b> (Plantlife)
+        </P>
+        <P skipTranslation>
+          <b>David Roy</b> (UK Centre for Ecology & Hydrology)
+        </P>
+        <P skipTranslation>
+          <b>Karolis Kazlauskis</b> (Flumens)
+        </P>
+        <P skipTranslation>
+          <b>John van Breda</b> (Biodiverse IT)
+        </P>
+        <P skipTranslation>
+          <b>Biren Rathod</b> (the UK Centre for Ecology & Hydrology)
+        </P>
+      </Section>
+
+      <Section>
+        <H>App Development</H>
+        <P>
+          This app was hand crafted with love by{' '}
+          <a href="https://flumens.io" style={{ whiteSpace: 'nowrap' }}>
+            Flumens.
+          </a>{' '}
+          A technical consultancy that excels at building bespoke environmental
+          science and community focussed solutions. For suggestions and feedback
+          please do not hesitate to{' '}
+          <a href="mailto:enquiries%40flumens.io?subject=Dune%20Defender%20App">
+            contact us
+          </a>
+          .
+        </P>
+        <p className="flumens-logo">
+          <a href="https://flumens.io">
+            <img src="/images/flumens.png" alt="" />
+          </a>
+        </p>
+      </Section>
+
+      <Section>
+        <H>Graphics</H>
+        <P>
+          Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </P>
+      </Section>
+    </Main>
+  </Page>
+);
