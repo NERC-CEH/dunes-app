9a35d8830c7e09d17c304a48629981e8dfb46fae
Karolis Kazlauskis <karolis@flumens.io>
2020-05-15 15:47:17 +0100

    Add survey to sample object

--- src/common/models/sample.js
+++ src/common/models/sample.js
@@ -1,4 +1,5 @@
 import { Sample, Media } from '@apps';
+import surveys from 'common/config/surveys';
 import { modelStore } from './store';
 import Occurrence from './occurrence';
 
@@ -8,6 +9,15 @@ export default class AppSample extends Sample {
   keys = () => {
     return { ...Sample.keys };
   };
+
+  constructor(...args) {
+    super(...args);
+
+    this.survey = surveys[this.metadata.survey];
+    if (!this.survey) {
+      throw new Error('No survey config was found');
+    }
+  }
 
   static fromJSON(json) {
     return super.fromJSON(json, Occurrence, AppSample, Media);
