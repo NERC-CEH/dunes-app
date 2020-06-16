584358fc0534ac79ea1906bdb617a68c28c55d3f
Karolis Kazlauskis <karolis@flumens.io>
2020-06-16 14:38:28 +0100

    Add survey info notes

--- src/Survey/FixedPhotography/Home/Main.js
+++ src/Survey/FixedPhotography/Home/Main.js
@@ -1,9 +1,19 @@
 import { observer } from 'mobx-react';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { IonList } from '@ionic/react';
 import { locationOutline } from 'ionicons/icons';
-import { Main, MenuAttrItem, MenuAttrItemFromModel } from '@apps';
-import PropTypes from 'prop-types';
+import {
+  Main,
+  MenuAttrItem,
+  MenuAttrItemFromModel,
+  MenuNote,
+  InfoButton,
+  Section,
+} from '@apps';
+import { Trans as T } from 'react-i18next';
+
+const { P } = Section;
 
 @observer
 class Component extends React.Component {
@@ -14,6 +24,7 @@ class Component extends React.Component {
 
   render() {
     const { sample, match } = this.props;
+    const survey = sample.getSurvey();
 
     const transect = (sample.attrs.location || {}).name;
 
@@ -31,6 +42,17 @@ class Component extends React.Component {
             icon={locationOutline}
             wrapText
           />
+
+          <MenuNote skipTranslation>
+            <T>You can find this survey information in our volunteer manual.</T>
+            <InfoButton label="open survey manual" header={survey.label}>
+              <Section>
+                <P>
+                  <i>TODO:</i> this is work in progress.
+                </P>
+              </Section>
+            </InfoButton>
+          </MenuNote>
         </IonList>
       </Main>
     );
