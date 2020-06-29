9e8d53637ee0292eb77e66341fdd39909bb2db51
Karolis Kazlauskis <karolis@flumens.io>
2020-06-29 22:45:46 +0100

    Refactor home page

--- src/Home/Home/index.js
+++ src/Home/Home/index.js
@@ -28,10 +28,35 @@ function index() {
       </IonHeader>
 
       <Main>
+        <div className="slide-header">
+          <div className="custom-shape-divider-bottom-1593438501">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                className="shape-fill"
+              />
+            </svg>
+          </div>
+        </div>
+
         <img className="app-logo" src="/images/appLogo.png" alt="" />
-        <div className="background-image" />
 
         <IonItemGroup>
+          <IonItem className="language-button" detail routerLink="/info/about">
+            <IonLabel>
+              <T>About</T>
+            </IonLabel>
+          </IonItem>
+          <IonItem className="language-button" detail routerLink="/info/manual">
+            <IonLabel>
+              <T>Citizen Science Manual</T>
+            </IonLabel>
+          </IonItem>
           <IonItem
             className="language-button"
             detail
