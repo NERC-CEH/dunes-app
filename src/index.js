ded87c0ddc7edb7cddf5fb2b6f580563f468cd50
Karolis Kazlauskis <karolis@flumens.io>
2020-07-10 19:43:49 +0100

    Fix ios statusbar

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import appModel from 'appModel';
 import userModel from 'userModel';
 import savedSamples from 'savedSamples';
 import initAnalytics from 'helpers/analytics';
+import { Plugins, StatusBarStyle } from '@capacitor/core';
 import App from './App';
+
+const { StatusBar } = Plugins;
+
+StatusBar.setStyle({
+  style: StatusBarStyle.Dark,
+});
 
 async function init() {
   await appModel._init;
