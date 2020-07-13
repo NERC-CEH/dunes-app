c2e081f867ecce38e095413bed7f915f42e10fd9
Karolis Kazlauskis <karolis@flumens.io>
2020-07-13 20:27:15 +0100

    Make autonavigate to new point

--- src/Survey/DunesProfile/Location/index.js
+++ src/Survey/DunesProfile/Location/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
+import { NavContext } from '@ionic/react';
 import { Page, Header } from '@apps';
 import Sample from 'sample';
 import Main from './Main';
 
 @observer
 class Controller extends React.Component {
+  static contextType = NavContext;
+
   static propTypes = {
     sample: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
   };
 
   addLocationPoint = type => {
-    const { sample } = this.props;
+    const { sample, location } = this.props;
     const survey = sample.getSurvey();
 
     const pointSample = survey.smp.create(Sample, type);
-
     sample.samples.push(pointSample);
+    pointSample.startGPS();
     sample.save();
 
-    pointSample.startGPS();
+    this.context.navigate(`${location.pathname}/${pointSample.cid}`);
   };
 
   render() {
