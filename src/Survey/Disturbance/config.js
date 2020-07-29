f4eda21be044080e2ea44871ddafeaa0dfd0bc13
Karolis Kazlauskis <karolis@flumens.io>
2020-07-29 13:21:52 +0100

    Add disturbance warehouse survey

--- src/Survey/Disturbance/config.js
+++ src/Survey/Disturbance/config.js
@@ -2,19 +2,14 @@ import * as Yup from 'yup';
 import userModel from 'userModel';
 import disturbance from 'common/images/disturbance.svg';
 import disturbanceBlack from './Home/Main/disturbanceIcon.svg';
-import {
-  dateAttr,
-  commentAttr,
-  surveyorsAttr,
-  verifyLocationSchema,
-} from '../common/config';
+import { dateAttr, commentAttr, surveyorsAttr } from '../common/config';
 
 const survey = {
   name: 'disturbance',
   label: 'Record Disturbance',
   icon: disturbance,
 
-  id: -1,
+  id: 596, // dev 596
   render: [],
 
   attrs: {
@@ -32,31 +27,31 @@ const survey = {
     },
 
     disturbance: {
-      id: -1,
+      id: 1500,
       type: 'checkbox',
       label: 'Disturbance',
       icon: disturbanceBlack,
       info: 'Please select the disturbance type for this record.',
       values: [
         { isPlaceholder: true, label: 'Bare sand/poached area' },
-        { value: 'Less than 1 metre across', id: -1 },
-        { value: 'More than 1 metre across', id: -1 },
+        { value: 'Less than 1 metre across', id: 17808 }, // dev 17804
+        { value: 'More than 1 metre across', id: 17809 }, // dev 17805
 
         { isPlaceholder: true, label: 'Animal dung' },
-        { value: 'Rabbit', id: -1 },
-        { value: 'Cattle', id: -1 },
-        { value: 'Pony', id: -1 },
-        { value: 'Sheep', id: -1 },
-        { value: 'Unknown', id: -1 },
+        { value: 'Rabbit', id: 17810 }, // dev 17806
+        { value: 'Cattle', id: 17811 }, // dev 17807
+        { value: 'Pony', id: 17812 }, // dev 17808
+        { value: 'Sheep', id: 17813 }, // dev 17809
+        { value: 'Unknown', id: 17814 }, // dev 17810
 
         { isPlaceholder: true, label: 'Rabbit activity' },
-        { value: 'Scrape', id: -1 },
-        { value: 'Burrow', id: -1 },
+        { value: 'Scrape', id: 17815 }, // dev 17811
+        { value: 'Burrow', id: 17816 }, // dev 17812
 
         { isPlaceholder: true, label: 'Other' },
-        { value: 'Vehicle tracks', id: -1 },
-        { value: 'Litter/flytipping', id: -1 },
-        { value: 'Other (please add a comment)', id: -1 },
+        { value: 'Vehicle tracks', id: 17817 }, // dev 17813
+        { value: 'Litter/flytipping', id: 17818 }, // dev 17814
+        { value: 'Other (please add a comment)', id: 17819 }, // dev 17815
       ],
     },
   },
@@ -64,7 +59,10 @@ const survey = {
   verify(attrs) {
     try {
       const transectSchema = Yup.object().shape({
-        location: verifyLocationSchema,
+        location: Yup.object().shape({
+          latitude: Yup.number().required(),
+          longitude: Yup.number().required(),
+        }),
       });
 
       transectSchema.validateSync(attrs, { abortEarly: false });
@@ -91,6 +89,7 @@ const survey = {
 
       attrs: {
         surveyors,
+        location_type: 'latlon',
       },
     });
 
