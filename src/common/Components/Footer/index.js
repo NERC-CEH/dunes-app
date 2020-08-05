e8f1386c63872f51ab25cb21af4daaaefcd55f07
Karolis Kazlauskis <karolis@flumens.io>
2020-08-05 10:21:40 +0100

    Add modal to survey manual page

--- src/common/Components/Footer/index.js
+++ src/common/Components/Footer/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { IonFooter, IonList } from '@ionic/react';
-import { Section, MenuNote, InfoButton } from '@apps';
+import { MenuNote, InfoButton } from '@apps';
 import { Trans as T } from 'react-i18next';
 import './styles.scss';
-
-const { P } = Section;
 
 function Footer({ sample }) {
   const isDisabled = sample.isDisabled();
@@ -15,25 +13,13 @@ function Footer({ sample }) {
 
   const survey = sample.getSurvey();
 
-  let manual = (
-    <Section>
-      <P>
-        <i>TODO:</i> this is work in progress.
-      </P>
-    </Section>
-  );
-
-  if (survey.Manual) {
-    manual = <survey.Manual />;
-  }
-
   return (
     <IonFooter id="info-footer" mode="md">
       <IonList lines="none" className="info-footer-list">
         <MenuNote skipTranslation>
           <T>You can find this survey information in our volunteer manual.</T>
-          <InfoButton label="open survey manual" header={survey.label}>
-            {manual}
+          <InfoButton label="open survey manual" header={survey.label} modal>
+            <survey.Manual />
           </InfoButton>
         </MenuNote>
       </IonList>
