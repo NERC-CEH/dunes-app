725c01e649eb90616348a93116f7f64b2f8398f1
Karolis Kazlauskis <karolis@flumens.io>
2020-09-08 19:16:34 +0100

    Fix dipwell uploaded record editing

--- src/Survey/Dipwell/Location/index.jsx
+++ src/Survey/Dipwell/Location/index.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { Page, Header } from '@apps';
+import PropTypes from 'prop-types';
 import Main from './Main';
 
 @observer
 class Controller extends React.Component {
-  static propTypes = {};
+  static propTypes = {
+    sample: PropTypes.object.isRequired,
+  };
 
   render() {
+    const { sample } = this.props;
+    const isDisabled = sample.isDisabled();
+
     return (
       <Page id="survey-dipwell-transect-edit">
         <Header title="Location" />
-        <Main {...this.props} />
+        <Main {...this.props} isDisabled={isDisabled} />
       </Page>
     );
   }
