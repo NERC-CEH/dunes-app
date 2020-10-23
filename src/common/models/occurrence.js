e378ad2b25ea20bf002d13fb0901b088c8ede920
Karolis Kazlauskis <karolis@flumens.io>
2020-10-23 18:05:13 +0100

    #133 Remove empty occurrence upload

--- src/common/models/occurrence.js
+++ src/common/models/occurrence.js
@@ -23,6 +23,29 @@ export default class AppOccurrence extends Occurrence {
     return this.parent.isDisabled();
   }
 
+  _checkIfEmptyPlantQuadrat() {
+    const isPlantQuadratSurvey =
+      this.parent.metadata.survey === 'plant-quadrat';
+    if (isPlantQuadratSurvey) {
+      const isEmptyOccurrence =
+        !Number.isFinite(this.attrs.health) &&
+        !Number.isFinite(this.attrs.nitrogen);
+
+      if (isEmptyOccurrence) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getSubmission() {
+    if (this._checkIfEmptyPlantQuadrat()) {
+      return null;
+    }
+
+    return super.getSubmission();
+  }
+
   // eslint-disable-next-line class-methods-use-this
   validateRemote() {
     return null;
